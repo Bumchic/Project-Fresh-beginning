60c19fc93eb3664a239ea333e5a86a5c9253986f
Bumchic <duonganhminh05@gmail.com>
2024-08-09 14:58:46 +0700

    Set accelaration and jump adjustment

--- Project Fresh beginning/Assets/Movement_script.cs
+++ Project Fresh beginning/Assets/Movement_script.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class Movement_script : MonoBehaviour
 {
     public Rigidbody2D Body;
     public float speed;
-    [Range(0.1f, 1f)]
-    public float friction;
+    public float JumpPower;
+    [Range(0.1f, 1f)] public float friction;
     public bool grounded;
     public BoxCollider2D FloorCheck;
     public LayerMask FloorCheckMask;
     float xinput;
     float yinput;
+    public float accelaration;
 
     void Groundcheck()
     {
@@ -21,25 +23,33 @@ public class Movement_script : MonoBehaviour
     void GetInput()
     {
         xinput = Input.GetAxis("Horizontal");
-        yinput  = Input.GetAxis("Vertical");
+        yinput = Input.GetAxis("Vertical");
     }
 
-    void MovementInput()
-    {
-        if (Mathf.Abs(xinput) > 0)
-        {
-            Body.velocity = new Vector2(xinput * speed, Body.velocity.y);
-        }
-        if (Mathf.Abs(yinput) > 0 && grounded)
-        {
-            Body.velocity = new Vector2(Body.velocity.x, yinput * speed);
-        }
-    }
+   
     void ApplyGroundFriction()
     {
         if (grounded && xinput == 0 && yinput == 0)
         {
             Body.velocity = Body.velocity * friction;
+        }
+    }
+    void MovementInput()
+    {
+        if (Mathf.Abs(xinput) > 0)
+        {
+            float Increment = xinput * accelaration;
+            float RealSpeed = Mathf.Sign(xinput + Increment, -speed, speed);
+            Body.velocity = new Vector2(RealSpeed, Body.velocity.y);
+            float direction = Mathf.Sign(xinput);
+            transform.localScale = new Vector2(direction, 1);
+        }
+    }
+    void JumpMovement()
+    {
+         if (Mathf.Abs(yinput) > 0 && grounded)
+        {
+            Body.velocity = new Vector2(Body.velocity.x, yinput* JumpPower);
         }
     }
     void Start()
@@ -50,15 +60,14 @@ public class Movement_script : MonoBehaviour
     void Update() 
     {
         GetInput();
-        MovementInput();
+        JumpMovement();
+        
     }
-
-   
 
     void FixedUpdate()
     {
+        MovementInput();
         Groundcheck();
         ApplyGroundFriction();
-       
     }
 }
