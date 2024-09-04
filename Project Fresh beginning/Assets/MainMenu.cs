0b593e6a9e4866047f40ed102a03beaf9f113d07
DoThanhLoc2334 <locdothanh233@gmail.com>
2024-09-04 22:22:27 +0700

    FixNameCode

--- Project Fresh beginning/Assets/MainMenu.cs
+++ Project Fresh beginning/Assets/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     public void LoadGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Demo FightSceen");
     }
     public void ExitGame()
     {
