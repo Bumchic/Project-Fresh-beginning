3815bc33d88f03f9a1755e5632945dfcd63cfb43
hhuyyyyyy13012004 <hoanghuyn98@gmail.com>
2024-09-14 18:47:25 +0700

    health bar

---
+++ Project Fresh beginning/Assets/Sprites/Health Bar/HealthBar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image fillBar;
+    public TextMeshProUGUI valueText;
+
+    public void UdateBar(int currentValue, int maxValue)
+    {
+        fillBar.fillAmount = (float)currentValue / (float)maxValue;
+        valueText.text = currentValue.ToString() + " / " + maxValue.ToString();
+    }
+}
---
+++ Project Fresh beginning/Assets/Sprites/Health Bar/PlayerHealth.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int maxHealth;
+    int currentHealth;
+
+    public HealthBar healthBar;
+
+    public UnityEvent OnDeath;
+
+    private void OnEnable()
+    {
+        OnDeath.AddListener(Death);
+    }
+
+    private void OnDisable()
+    {
+        OnDeath.RemoveListener(Death);
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+
+        healthBar.UdateBar(currentHealth, maxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+            OnDeath.Invoke();
+        }
+        healthBar.UdateBar(currentHealth, maxHealth);
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TakeDamage(20);
+        }
+    }
+}
