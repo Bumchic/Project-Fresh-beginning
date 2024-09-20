41b66879a97c8e31aaf4900792e485f1a1e60596
Bumchic <duonganhminh05@gmail.com>
2024-09-20 22:15:39 +0700

    Added jump animation
    bugged as heck tho

--- Project Fresh beginning/Assets/Movement_script.cs
+++ Project Fresh beginning/Assets/Movement_script.cs
@@ -80,7 +80,7 @@ public class Movement_script : MonoBehaviour
     void MovementInput()
     {
  
-        SprintModifier();
+        //SprintModifier();
         if (Mathf.Abs(xinput) > 0)
         {
             Animator.SetBool("IsRunning", true);
@@ -104,20 +104,21 @@ public class Movement_script : MonoBehaviour
     {
             Body.velocity = new Vector2(Body.velocity.x, JumpPower);
     }
-
     void JumpMovement()
     { 
-        if (grounded)
+      /*  if (grounded)
         {
-            JumpCounter = 1;
-        }
-        if ((JumpButton()) && JumpCounter > 0)
+            JumpCounter = 1; //This is the code for double jump :/
+        }*/
+        if ((JumpButton()) && grounded)
         {
             Jump();
-            JumpCounter--;
-        }
-    }
 
+            //JumpCounter--;
+        }
+        Animator.SetFloat("IsInAir", Body.velocity.y);
+    }
+    
     bool CrouchButtonPressed()
     {
         return Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
