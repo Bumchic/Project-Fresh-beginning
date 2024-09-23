665862fad97c88336ce9012d450209b774cd33db
DoThanhLoc2334 <locdothanh233@gmail.com>
2024-09-23 12:07:00 +0700

    HomeButton in level Scene

--- Project Fresh beginning/Assets/MainMenu.cs
+++ Project Fresh beginning/Assets/MainMenu.cs
@@ -3,20 +3,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public void LoadGame()
-    {
-        SceneManager.LoadScene("Demo FightSceen");
-    }
-
-   // public void LoadGame()
-   //{
-    //    SceneManager.LoadScene("level");
-   // }
-
+    //public void LoadGame()
+    //{
+    //    SceneManager.LoadScene("Demo FightSceen");
+    //}
     public void BackMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
+    public void LoadGame()
+   {
+        SceneManager.LoadScene("level");
+   }
+
+    //public void BackMenu()
+    //{
+    //    SceneManager.LoadScene("MainMenu");
+    //}
     public void ExitGame()
     {
         Application.Quit(); 
