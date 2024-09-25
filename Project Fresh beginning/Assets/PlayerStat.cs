2135e446f4b97ea59c572cf0fca4d072cba8afdb
Bumchic <duonganhminh05@gmail.com>
2024-09-25 20:55:13 +0700

    Reapply "Add HealthBar"
    This reverts commit 86f1c4c5a0b146f03cc2d4ed26caccbf4450df64.

--- Project Fresh beginning/Assets/PlayerStat.cs
+++ Project Fresh beginning/Assets/PlayerStat.cs
@@ -1,47 +1,103 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerStat : MonoBehaviour
 {
- 
-    void Start()
+    // Declare variables inside the class
+    [SerializeField] private int maxHealth;
+    private int curentHealth;
+
+    // Reference to the health bar UI component
+    public HealthBar healthBar;
+
+    // Event triggered when the player dies
+    public UnityEvent OnDeath;
+
+    private void OnEnable()
     {
-        
+        // Add a listener to the death event
+        OnDeath.AddListener(Death);
+    }
+
+    private void OnDisable()
+    {
+        // Remove listener to prevent errors when the object is disabled
+        OnDeath.RemoveListener(Death);
+    }
+
+    private void Start()
+    {
+        // Correctly reference PlayerHealth from GameManager
+        curentHealth = GameManager.gameManager.PlayerHealth.currentHealth;
+        maxHealth = GameManager.gameManager.PlayerHealth.maxHealth;
+
+        healthBar.UpdateBar(curentHealth, maxHealth);
     }
 
 
-    void Update()
+    public void TakeDamage(int damage)
     {
-        if(Input.GetKeyDown(KeyCode.Alpha0))
+        // Decrease health and update UI
+        curentHealth -= damage;
+
+        if (curentHealth <= 0)
         {
-            PlayerTakeDamage(10);
-            Debug.Log(GameManager.gameManager.PlayerHealth.currentHealth);
+            curentHealth = 0;
+            OnDeath.Invoke(); // Trigger the death event
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha9))
-            {
-            PlayerHeal(10);
-            Debug.Log(GameManager.gameManager.PlayerHealth.currentHealth);
-        }
-
-        gameOver();
+        healthBar.UpdateBar(curentHealth, maxHealth);
     }
-    private void gameOver()
+
+    public void Heal(int healAmount)
     {
-        if (GameManager.gameManager.PlayerHealth.currentHealth <= 0)
+        // Increase health and update UI
+        curentHealth += healAmount;
+
+        if (curentHealth > maxHealth)
+        {
+            curentHealth = maxHealth;
+        }
+
+        healthBar.UpdateBar(curentHealth, maxHealth);
+    }
+
+    private void Death()
+    {
+        // Destroy the player object on death
+        Destroy(gameObject);
+        // Trigger game over functionality (e.g., return to menu)
+        GameOver.LoadMainMenu();
+    }
+
+    private void Update()
+    {
+        // Press '9' to take damage
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            TakeDamage(10);
+            Debug.Log("Current Health: " + curentHealth);
+        }
+
+        // Press '0' to heal
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            Heal(10);
+            Debug.Log("Current Health: " + curentHealth);
+        }
+
+        // Check if the player is dead
+        GameOverCheck();
+    }
+
+    private void GameOverCheck()
+    {
+        // Check if health is 0 or below and trigger game over
+        if (curentHealth <= 0)
         {
             GameOver.LoadMainMenu();
         }
-    }
-    private void PlayerTakeDamage(int DamageAmount)
-    {
-        GameManager.gameManager.PlayerHealth.TakeDamage(DamageAmount);       
-    }
-
-    private void PlayerHeal(int HealAmount)
-    {
-        GameManager.gameManager.PlayerHealth.Heal(HealAmount);
-
     }
 }
---
+++ Project Fresh beginning/Assets/Sprites/Health Bar/PlayerHealth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int maxHealth;
+    int curentHealth;
+
+    public HealthBar healthBar;
+    public UnityEvent OnDeath;
+    private void OnEnable()
+    {
+        OnDeath.AddListener(Death);
+    }
+    private void OnDisable()
+    {
+        OnDeath.RemoveListener(Death);
+    }
+    private void Start()
+    {
+        curentHealth = maxHealth;
+
+        healthBar.UpdateBar(curentHealth, maxHealth);
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        curentHealth -= damage;
+
+        if (curentHealth < 0)
+        {
+            curentHealth = 0;
+            OnDeath.Invoke();
+        }
+        healthBar.UpdateBar(curentHealth, maxHealth);
+    }
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+
+    }
+}
