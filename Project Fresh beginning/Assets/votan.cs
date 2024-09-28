d3aa1635e0468b7ebf2328fefe0a6903e03966cb
chuatehaonam <hihi36hihi@gmail.com>
2024-09-28 12:17:36 +0700

    mapp
    da lam nho lai 0.2

--- Project Fresh beginning/Assets/votan.cs
+++ Project Fresh beginning/Assets/votan.cs
@@ -2,33 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class votan : MonoBehaviour
+public class Votan : MonoBehaviour
 {
     public Transform MainCam;
     public Transform map1;
     public Transform map2;
     public float length;
 
-    // Update is called once per frame
+   
     void Update()
     {
         // Check if MainCam is to the right of map1
         if (MainCam.position.x > map1.position.x)
         {
-            update(Vector3.right);
-
+            UpdateMapPosition(Vector3.right);
         }
         else if (MainCam.position.x < map1.position.x) // MainCam is to the left of map1
         {
-            update(Vector3.left); // Always move map1 to the left (redundant)
+            UpdateMapPosition(Vector3.left);
         }
     }
 
-    void update(Vector3 direction)
+    void UpdateMapPosition(Vector3 direction)
     {
-        map2.position = map1.position + direction * length; // Update map2 position
-        Transform temp = map1;  // Store map1 in a temporary variable
-        map1 = map2;           // Swap map1 and map2 references
-        map2 = temp;           // Restore map2 reference from temporary variable
+        map2.position = map1.position + direction * length;
+        Transform temp = map1;
+        map1 = map2;
+        map2 = temp;
     }
 }
