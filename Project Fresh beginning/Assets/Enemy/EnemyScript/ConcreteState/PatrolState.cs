b2b8333c7afeba19c06b7faf234ec27b38696531
Bumchic <duonganhminh05@gmail.com>
2024-10-25 16:07:24 +0700

    Added StateMachine for enemy

---
+++ Project Fresh beginning/Assets/Enemy/EnemyScript/ConcreteState/PatrolState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState : EnemyState
+{
+    public PatrolState(EnemyBaseScript enemy, EnemyStateMachine stateMachine) : base(enemy, stateMachine)
+    {
+
+    }
+    public override void EnterState()
+    {
+        base.EnterState();
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+    }
+
+    public override void FrameUpdate()
+    {
+        base.FrameUpdate();
+    }
+
+    public override void PhysicUpdate()
+    {
+        base.PhysicUpdate();
+    }
+}
--- Project Fresh beginning/Assets/Enemy/EnemyScript/EnemyBaseScript/EnemyBaseScript.cs
+++ Project Fresh beginning/Assets/Enemy/EnemyScript/EnemyBaseScript/EnemyBaseScript.cs
@@ -1,23 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
-public class EnemyBaseScript : MonoBehaviour, IEnemyMoveable
+public class EnemyBaseScript : MonoBehaviour
 {
     [field:SerializeField]public Rigidbody2D rigidbody { get; set ; }
+    public EnemyStateMachine stateMachine { get; set; }
+    public PatrolState patrolState { get; set; }
+    public float acceleration { get; set; } = 1f;
     void Awake()
     {
-        GetComponent<Renderer>().material.color = Color.red;
+        stateMachine = new EnemyStateMachine();
+        patrolState = new PatrolState(this, stateMachine);
     }
+
+    void Start()
+    {
+        stateMachine.Initialize(patrolState);
+    }
+
+    void Update()
+    {
+        stateMachine.enemyState.FrameUpdate();
+    }
+
+    void FixedUpdate()
+    {
+        stateMachine.enemyState.PhysicUpdate();
+    }
+
 
     public void FaceDirection()
     {
        
     }
 
-    public void Move()
+    public void Move(float speed)
     {
+
         
+        float CurrentSpeed = Mathf.Clamp(rigidbody.velocity.x + acceleration, -speed, +speed);
+
+        rigidbody.velocity = new Vector2(CurrentSpeed, rigidbody.velocity.y);
+        FaceDirection();
     }
 
     public void PathFind()
