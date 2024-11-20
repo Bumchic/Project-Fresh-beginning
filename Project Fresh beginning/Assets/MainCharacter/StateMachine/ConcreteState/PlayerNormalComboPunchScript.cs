24e86425ca7e98a00d0afec8a93bcf3318ffab10
Bumchic <duonganhminh05@gmail.com>
2024-11-20 22:02:24 +0700

    ToDo: Remake punch script

---
+++ Project Fresh beginning/Assets/MainCharacter/StateMachine/ConcreteState/PlayerNormalComboPunchScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNormalComboPunchScript : PlayerState
+{
+    public PlayerNormalComboPunchScript(Player player, PlayerStateMachine playerStateMachine) : base(player, playerStateMachine)
+    {
+
+    }
+    public override void AnimationTriggerEvent(Player.AnimationTriggerType triggertype)
+    {
+        base.AnimationTriggerEvent(triggertype);
+    }
+
+    public override void EnterState()
+    {
+        base.EnterState();
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+    }
+
+    public override void FrameUpdate()
+    {
+        base.FrameUpdate();
+    }
+
+    public override void PhysicUpdate()
+    {
+        base.PhysicUpdate();
+    }
+}
