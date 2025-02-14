3bff1b52d2a29055103e1122bf859b9f0b3336b5
Bumchic <duonganhminh05@gmail.com>
2025-02-15 00:56:19 +0700

    Update Idle state for grunt

--- Project Fresh beginning/Assets/Enemy/EnemyScript/ConcreteState/IdleState.cs
+++ Project Fresh beginning/Assets/Enemy/EnemyScript/ConcreteState/IdleState.cs
@@ -1,16 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class IdleState : EnemyState
 {
+    private CancellationTokenSource cts;
+    private CancellationToken ct;
+    private bool finishedWaiting;
+    Thread thread;
     public IdleState(EnemyBaseScript enemy, EnemyStateMachine stateMachine): base(enemy, stateMachine)
     {
-
+        cts = new CancellationTokenSource();
+        finishedWaiting = false;
+        thread = Thread.CurrentThread;
     }
     public override void EnterState()
     {
         base.EnterState();
+        
+        CancellationToken ct = cts.Token;
+        try
+        {
+            Task.Run(() =>
+            {
+                Task.Delay(2000).Wait(ct);
+                Debug.Log(ct.IsCancellationRequested);
+                if (ct.IsCancellationRequested)
+                {
+                    ct.ThrowIfCancellationRequested();
+                }
+                Dispatcher.Enqueue(() =>
+                {
+                    enemy.stateMachine.ChangeState(enemy.patrolState);
+                }); 
+            }, ct);
+        } catch(OperationCanceledException)
+        {
+
+        }
     }
 
     public override void ExitState()
@@ -21,6 +53,11 @@ public class IdleState : EnemyState
     public override void FrameUpdate()
     {
         base.FrameUpdate();
+        if(enemy.isChasingPlayer)
+        {
+            cts.Cancel();
+            enemy.stateMachine.ChangeState(enemy.chasingPlayerState);
+        }
     }
 
     public override void PhysicUpdate()
